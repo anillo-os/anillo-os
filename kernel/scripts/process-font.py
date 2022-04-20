fcfbd3ad160226bf405686facf567deac55fe8c7
Ariel Abreu <facekapow@outlook.com>
2022-04-20 13:13:48 -0400

    scripts: Support PSF1 fonts as well

--- kernel/scripts/process-font.py
+++ kernel/scripts/process-font.py
@@ -20,44 +20,72 @@ OUTPUT_PATH = sys.argv[2]
 
 HEADER_GUARD_NAME = '_GEN_FERRO_FONT_H'
 
+PSF1_MAGIC = bytearray([0x36, 0x04])
 PSF2_MAGIC = bytearray([0x72, 0xb5, 0x4a, 0x86])
-PSF_UNICODE_FLAG = 1
+PSF2_UNICODE_FLAG = 0x01
+
+PSF1_512_FLAG = 0x01
+PSF1_UNICODE_FLAG = 0x02
 
 contents = []
 with open(FONT_PATH, 'rb') as file:
 	contents = bytearray(file.read())
 
-header = struct.unpack('4BIIIIIII', contents[0:32])
-magic = bytearray(header[0:4])
-version = header[4]
-header_size = header[5]
-flags = header[6]
-glyph_count = header[7]
-glyph_size = header[8]
-glyph_height = header[9]
-glyph_width = header[10]
+magic = bytearray(contents[0:2])
 
-if magic != PSF2_MAGIC:
-	print('Invalid PSF2 magic')
-	exit(1)
+if magic != PSF1_MAGIC:
+	magic = bytearray(contents[0:4])
+	if magic != PSF2_MAGIC:
+		print('Invalid PSF magic')
+		exit(1)
+
+version = 0
+header_size = 0
+flags = 0
+glyph_count = 0
+glyph_size = 0
+glyph_height = 0
+glyph_width = 0
+
+if magic == PSF1_MAGIC:
+	header = struct.unpack('BB', contents[2:4])
+	header_size = 4
+	flags = header[0]
+	glyph_count = 512 if (flags & PSF1_512_FLAG) != 0 else 256
+	glyph_size = header[1]
+	glyph_width = 8
+	glyph_height = glyph_size
+elif magic == PSF2_MAGIC:
+	header = struct.unpack('IIIIIII', contents[4:32])
+	version = header[0]
+	header_size = header[1]
+	flags = header[2]
+	glyph_count = header[3]
+	glyph_size = header[4]
+	glyph_height = header[5]
+	glyph_width = header[6]
 
 table_offset = header_size + (glyph_size * glyph_count)
 unicode_map = [0] * 0xffff
+font_data = contents[0:table_offset]
 
-if flags & PSF_UNICODE_FLAG:
+if magic == PSF1_MAGIC:
+	# Ferro expects a PSF2 font; let's convert the header to a PSF2 header
+	new_header = struct.pack('4BIIIIIII', PSF2_MAGIC[0], PSF2_MAGIC[1], PSF2_MAGIC[2], PSF2_MAGIC[3], version, struct.calcsize('4BIIIIIII'), PSF2_UNICODE_FLAG if (flags & PSF1_UNICODE_FLAG) != 0 else 0, glyph_count, glyph_size, glyph_height, glyph_width)
+	font_data = new_header + font_data[header_size:]
+
+if magic == PSF2_MAGIC and (flags & PSF2_UNICODE_FLAG) != 0:
 	table = contents[table_offset:]
 
 	# general process derived from https://wiki.osdev.org/PC_Screen_Font
 
 	table_index = 0
 	glyph_index = 0
-	while table_index < len(table):
+
+	def read_utf8():
 		curr = table[table_index]
-		if curr == 0xff:
-			table_index += 1
-			glyph_index += 1
-			continue
-		elif curr & 0x80:
+
+		if curr & 0x80:
 			if (curr & 0x20) == 0:
 				curr = (((table[table_index] & 0x1f) << 6) | (table[table_index + 1] & 0x3f)) & 0xffff
 				table_index += 1
@@ -69,8 +97,59 @@ if flags & PSF_UNICODE_FLAG:
 				table_index += 3
 			else:
 				curr = 0
-		unicode_map[curr] = glyph_index
+
 		table_index += 1
+
+		return curr
+
+	while table_index < len(table):
+		curr = table[table_index]
+
+		if curr == 0xff:
+			# handle the terminator
+			table_index += 1
+			glyph_index += 1
+		elif curr == 0xfe:
+			# skip combining symbols
+			while curr != 0xff:
+				if curr == 0xfe:
+					table_index += 1
+					curr = table[table_index]
+				else:
+					read_utf8()
+					curr = table[table_index]
+
+			# now handle the terminator
+			table_index += 1
+			glyph_index += 1
+		else:
+			curr = read_utf8()
+			unicode_map[curr] = glyph_index
+elif magic == PSF1_MAGIC and (flags & PSF1_UNICODE_FLAG) != 0:
+	table = contents[table_offset:]
+
+	table_index = 0
+	glyph_index = 0
+
+	while table_index < len(table):
+		curr = struct.unpack_from('<H', table, table_index)[0]
+
+		if curr == 0xffff:
+			# handle the terminator
+			table_index += 2
+			glyph_index += 1
+		elif curr == 0xfffe:
+			# skip combining symbols
+			while curr != 0xffff:
+				table_index += 2
+				curr = struct.unpack_from('<H', table, table_index)[0]
+
+			# now handle the terminator
+			table_index += 2
+			glyph_index += 1
+		else:
+			unicode_map[curr] = glyph_index
+			table_index += 2
 else:
 	unicode_map = []
 
@@ -78,6 +157,6 @@ anillo_util.mkdir_p(os.path.dirname(OUTPUT_PATH))
 
 with open(OUTPUT_PATH, 'wb') as outfile:
 	outfile.write(('#ifndef ' + HEADER_GUARD_NAME + '\n#define ' + HEADER_GUARD_NAME + '\n\n#include <stdint.h>\n\n').encode())
-	outfile.write(anillo_util.to_c_array('font_data', contents[0:table_offset]).encode())
+	outfile.write(anillo_util.to_c_array('font_data', font_data).encode())
 	outfile.write(anillo_util.to_c_array('unicode_map', unicode_map, 'uint16_t', lambda v: anillo_util.to_padded_hex(v, 4)).encode())
 	outfile.write(('\n#endif // ' + HEADER_GUARD_NAME + '\n').encode())
