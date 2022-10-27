bc513160d040ab33cfeb33655559fe85ae4179e7
Ariel Abreu <facekapow@outlook.com>
2022-10-27 14:03:49 -0400

    libsys: Add support for shared memory and advanced page operations

--- libsys/include/libsys/pages.h
+++ libsys/include/libsys/pages.h
@@ -24,12 +24,33 @@
 
 #include <libsys/base.h>
 #include <ferro/error.h>
+#include <libsys/objects.h>
 
 LIBSYS_DECLARATIONS_BEGIN;
 
-LIBSYS_WUR ferr_t sys_page_allocate(size_t page_count, uint64_t flags, void** out_address);
+LIBSYS_OBJECT_CLASS(shared_memory);
+
+LIBSYS_OPTIONS(uint64_t, sys_shared_memory_flags) {
+	sys_shared_memory_flag_xxx_reserved = 0
+};
+
+LIBSYS_OPTIONS(uint64_t, sys_page_flags) {
+	sys_page_flag_contiguous  = 1 << 0,
+	sys_page_flag_prebound    = 1 << 1,
+	sys_page_flag_unswappable = 1 << 2,
+	sys_page_flag_uncacheable = 1 << 3,
+};
+
+LIBSYS_WUR ferr_t sys_page_allocate(size_t page_count, sys_page_flags_t flags, void** out_address);
+LIBSYS_WUR ferr_t sys_page_allocate_advanced(size_t page_count, uint8_t alignment_power, sys_page_flags_t flags, void** out_address);
 
 LIBSYS_WUR ferr_t sys_page_free(void* address);
+
+LIBSYS_WUR ferr_t sys_page_translate(const void* address, uint64_t* out_physical_address);
+
+LIBSYS_WUR ferr_t sys_shared_memory_allocate(size_t page_count, sys_shared_memory_flags_t flags, sys_shared_memory_t** out_shared_memory);
+LIBSYS_WUR ferr_t sys_shared_memory_map(sys_shared_memory_t* shared_memory, size_t page_count, size_t page_offset_count, void** out_address);
+LIBSYS_WUR ferr_t sys_shared_memory_bind(sys_shared_memory_t* shared_memory, size_t page_count, size_t page_offset_count, void* address);
 
 LIBSYS_ALWAYS_INLINE uintptr_t sys_page_round_up_multiple(uintptr_t number) {
 	return (number + (4096ULL - 1)) & -4096ULL;
@@ -37,6 +58,14 @@ LIBSYS_ALWAYS_INLINE uintptr_t sys_page_round_up_multiple(uintptr_t number) {
 
 LIBSYS_ALWAYS_INLINE uintptr_t sys_page_round_up_count(uintptr_t number) {
 	return sys_page_round_up_multiple(number) / 4096ULL;
+};
+
+LIBSYS_ALWAYS_INLINE uintptr_t sys_page_round_down_multiple(uintptr_t number) {
+	return number & -4096ULL;
+};
+
+LIBSYS_ALWAYS_INLINE uintptr_t sys_page_round_down_count(uintptr_t number) {
+	return number / 4096ULL;
 };
 
 LIBSYS_DECLARATIONS_END;
--- libsys/src/pages.c
+++ libsys/src/pages.c
@@ -16,13 +16,85 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <libsys/pages.h>
+#include <libsys/pages.private.h>
 #include <gen/libsyscall/syscall-wrappers.h>
 
-ferr_t sys_page_allocate(size_t page_count, uint64_t flags, void** out_address) {
-	return libsyscall_wrapper_page_allocate_any(page_count, flags, out_address);
+static void sys_shared_memory_destroy(sys_object_t* obj) {
+	sys_shared_memory_object_t* shared_memory = (void*)obj;
+	if (shared_memory->did != UINT64_MAX) {
+		LIBSYS_WUR_IGNORE(libsyscall_wrapper_page_close_shared(shared_memory->did));
+	}
+};
+
+static const sys_object_class_t shared_memory_class = {
+	LIBSYS_OBJECT_CLASS_INTERFACE(NULL),
+	.destroy = sys_shared_memory_destroy,
+};
+
+const sys_object_class_t* sys_object_class_shared_memory(void) {
+	return &shared_memory_class;
+};
+
+ferr_t sys_page_allocate(size_t page_count, sys_page_flags_t flags, void** out_address) {
+	return sys_page_allocate_advanced(page_count, 0, flags, out_address);
+};
+
+ferr_t sys_page_allocate_advanced(size_t page_count, uint8_t alignment_power, sys_page_flags_t flags, void** out_address) {
+	return libsyscall_wrapper_page_allocate(page_count, flags, alignment_power, out_address);
 };
 
 ferr_t sys_page_free(void* address) {
 	return libsyscall_wrapper_page_free(address);
+};
+
+ferr_t sys_page_translate(const void* address, uint64_t* out_physical_address) {
+	return libsyscall_wrapper_page_translate(address, out_physical_address);
+};
+
+ferr_t sys_shared_memory_allocate(size_t page_count, sys_shared_memory_flags_t flags, sys_shared_memory_t** out_shared_memory) {
+	ferr_t status = ferr_ok;
+	sys_shared_memory_object_t* shared_memory = NULL;
+
+	status = sys_object_new(&shared_memory_class, sizeof(*shared_memory) - sizeof(shared_memory->object), (void*)&shared_memory);
+	if (status != ferr_ok) {
+		goto out;
+	}
+
+	shared_memory->did = UINT64_MAX;
+
+	status = libsyscall_wrapper_page_allocate_shared(page_count, 0, &shared_memory->did);
+
+out:
+	if (status == ferr_ok) {
+		*out_shared_memory = (void*)shared_memory;
+	} else {
+		if (shared_memory) {
+			sys_release((void*)shared_memory);
+		}
+	}
+	return status;
+};
+
+ferr_t sys_shared_memory_map(sys_shared_memory_t* obj, size_t page_count, size_t page_offset_count, void** out_address) {
+	ferr_t status = ferr_ok;
+	sys_shared_memory_object_t* shared_memory = (void*)obj;
+	void* address = NULL;
+
+	status = libsyscall_wrapper_page_map_shared(shared_memory->did, page_count, page_offset_count, 0, 0, &address);
+
+out:
+	if (status == ferr_ok) {
+		*out_address = address;
+	}
+	return status;
+};
+
+ferr_t sys_shared_memory_bind(sys_shared_memory_t* obj, size_t page_count, size_t page_offset_count, void* address) {
+	ferr_t status = ferr_ok;
+	sys_shared_memory_object_t* shared_memory = (void*)obj;
+
+	status = libsyscall_wrapper_page_bind_shared(shared_memory->did, page_count, page_offset_count, address);
+
+out:
+	return status;
 };
