3a9931deae3e5cf919f2dab2dbd9c6516c381728
Ariel Abreu <facekapow@outlook.com>
2025-10-25 22:13:50 -0400

    [rust] Add missing script for linking
    This was from the previous `rust` branch but was missed when transferring the build framework over to this branch.

---
+++ scripts/linker-wrapper.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+from typing import List
+import os
+
+args = sys.argv[1:]
+final_args: List[str] = []
+
+arch = 'unknown'
+skip_next = False
+
+for index, arg in enumerate(args):
+	if arg == '-arch':
+		arch = args[index + 1]
+	elif arg == '-flavor':
+		skip_next = True
+		continue
+	elif arg == '--eh-frame-hdr':
+		continue
+	elif arg == '-dead_strip':
+		continue
+	elif skip_next:
+		skip_next = False
+		continue
+
+	final_args += [arg]
+
+if arch == 'arm64':
+	arch = 'aarch64'
+
+os.execlp(f'{arch}-apple-darwin-ld', f'{arch}-apple-darwin-ld', *final_args)
